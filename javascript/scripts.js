d6e4c80ee75964d9aebc88372f0b11cd024840e2
Samad Chan <samad.chan@verafin.com>
2019-08-10 13:48:11 -0230

    added header and footer using jquery, made resume look cool

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,10 @@
 //JQuery Example
 $(document).ready(function(){
-	$(function(){
-		$("#header").load("../header.html");
-		$("#footer").load("../footer.html");
-	});
+	$("#header").load("reusables/header.html");
+	$("#footer").load("reusables/footer.html");
+	$('#social-links').load("reusables/social-links.html");
 });
+
+$('.nav-item').click(function(){
+	$(this).tab('active');
+  })
