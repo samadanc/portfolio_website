990b0cf8dc2e2465c343080a6f7ac3ef22479a7d
Samad Chan <samad.chan@verafin.com>
2019-12-25 05:13:59 -0330

    prevented arrow scrolling

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -9,6 +9,12 @@ $('.nav-item').click(function(){
 	$(this).tab('active');
   })
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 // if(window.location.protocol != "https"){
 // 	window.location.href = window.location.href.replace('http', 'https');    
 // }
